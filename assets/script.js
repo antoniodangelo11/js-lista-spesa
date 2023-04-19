d22816ae83dc724a1e8b40ee1062b6673b8ef984
antoninodangelo11 <reapercsp@gmail.com>
2023-04-19 14:42:49 +0200

    add while cicle

--- assets/script.js
+++ assets/script.js
@@ -18,9 +18,20 @@ const arrList = [
 const eleList = document.querySelector('#list');
 
 // soluzione con il ciclo for
-for (let i = 0; i < arrList.length; i++) {
-	console.log(arrList[i]);
-	eleList.innerHTML += `<li>${arrList[i]}</li>`;
-}
+
+// for (let i = 0; i < arrList.length; i++) {
+// 	console.log(arrList[i]);
+// 	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+// }
 
 // soluzione con il ciclo while
+
+//Inizializzazione della variabile
+let i = 0;
+
+while (i < arrList.length) {
+    console.log(arrList[i]);
+    eleList.innerHTML += `<li>${arrList[i]}</li>`;
+    // Incremento della variabile all'interno per ultimo
+    i++;
+}
